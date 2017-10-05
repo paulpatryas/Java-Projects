6fb4eeb6c6acc504e3a5d95d4a8c2fad8b83bb09
paulpatryas <32423930+paulpatryas@users.noreply.github.com>
2017-10-05 16:09:20 -0400

    employee program

---
+++ src/Employee.java
@@ -0,0 +1,48 @@
+import java.util.Comparator;
+
+public class Employee implements Comparable<Employee> {
+	private String ssn;
+	private String lastName;
+	private String firstName;
+
+	public int compareTo(Employee emp) {
+		return Comparator.comparing(Employee::getFirstName)
+	              .thenComparing(Employee::getLastName)
+	              .thenComparing(Employee::getSSN)
+	              .compare(this, emp);
+	}
+
+	public Employee(String socialSN, String ln, String fn) {
+		ssn = socialSN;
+		lastName = ln;
+		firstName = fn;
+	}
+
+	public String toString() {
+		return "Employee[SSN= " + ssn + ", Last Name= " + lastName + ", First Name= " + firstName + "]";
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getSSN() {
+		return ssn;
+	}
+
+	public void setLastName(String setLN) {
+		lastName = setLN;
+	}
+
+	public void setFirstName(String setFN) {
+		firstName = setFN;
+	}
+
+	public void setSSN(String setSSN) {
+		ssn = setSSN;
+	}
+}
